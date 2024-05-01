9079c9d8789c3b8ed5f014a933f15b15554acf95
pestopasta74 <prestonwhiteman07@gmail.com>
2024-05-01 10:23:34 +0100

    literally just added new lines to make it cleaner

--- data_validation.py
+++ data_validation.py
@@ -7,30 +7,36 @@ class DataValidator:
         pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+(?:\.[a-zA-Z]{2,}|(?:\d{1,3}\.){3}\d{1,3})$'
         return bool(re.fullmatch(pattern, data))
 
+
     def phone(self, data):
         # Checks weather the number is a valid UK phone number
         pattern = r'^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$'
         return bool(re.fullmatch(pattern, data))
+
 
     def name(self, data):
         if re.match(r'^[a-zA-Z]+$', data):
             return True
         return False
 
+
     def username(self, data):
         if re.match(r'^[a-zA-Z0-9]+$', data):
             return True
         return False
+
 
     def birthdate(self, data):
         if re.match(r'^\d{4}-\d{2}-\d{2}$', data):
             return True
         return False
 
+
     def age(self, data):
         if re.match(r'^[0-9]+$', data) and 0 < int(data) < 150:
             return True
         return False
+
 
     def length_check(self, data, length, option):
         if option == 'min':
@@ -80,6 +86,7 @@ class TestValidator(unittest.TestCase):
         for email in invalid_emails:
             # Tests invalid emails
             self.assertFalse(self.validator.email(email))
+
 
     def test_valid_phone(self):
         # Valid phone numbers
