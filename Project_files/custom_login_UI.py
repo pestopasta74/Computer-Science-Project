d2477bce6e642f16eaa20fb7ea973e5a24071ce6
pestopasta74 <prestonwhiteman07@gmail.com>
2024-06-05 09:51:32 +0100

    Changed the size and places of widgets

--- Project_files/custom_login_UI.py
+++ Project_files/custom_login_UI.py
@@ -8,12 +8,13 @@ ctk.set_default_color_theme("blue")
 
 # Create the main application window
 root = ctk.CTk()
+root.resizable(False, False)
 
 class LoginUI:
     def __init__(self, master):
         self.master = master
         self.master.title('Login')
-        self.master.geometry('500x300')
+        self.master.geometry('400x250')
         self.validator = DataValidator()
 
         self.create_widgets()
@@ -26,18 +27,18 @@ class LoginUI:
         self.label_password = ctk.CTkLabel(self.master, text='Password')
         self.entry_password = ctk.CTkEntry(self.master, show='*', corner_radius=10, border_width=2, border_color='black')
 
-        self.submit_button = ctk.CTkButton(self.master, text='Submit', command=self.validate)
-        self.forgot_password_button = ctk.CTkButton(self.master, text='Forgotten password?')
+        self.submit_button = ctk.CTkButton(self.master, text='Submit', command=self.validate, fg_color='green')
+        self.forgot_password_button = ctk.CTkButton(self.master, text='Forgotten password?', text_color='black')
 
     def place_widgets(self):
-        self.label_email.place(x=100, y=50)
-        self.entry_email.place(x=200, y=50)
+        self.label_email.place(x=50, y=50)
+        self.entry_email.place(x=150, y=50)
 
-        self.label_password.place(x=100, y=100)
-        self.entry_password.place(x=200, y=100)
+        self.label_password.place(x=50, y=100)
+        self.entry_password.place(x=150, y=100)
 
-        self.submit_button.place(x=200, y=150)
-        self.forgot_password_button.place(x=200, y=200)
+        self.submit_button.place(x=250, y=210)
+        self.forgot_password_button.place(x=150, y=150)
 
     def validate(self):
         email = self.entry_email.get()
