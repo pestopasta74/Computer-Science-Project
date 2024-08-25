29e134257a4a83fb1fbe9a423154928398382e8f
pestopasta74 <prestonwhiteman07@gmail.com>
2024-08-25 04:30:43 +0100

    it doesnt work but i'm going to bed

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from modules.user_management import UserDatabase
 import subprocess
 
 try:
-    open("user_login_info.txt", "r")
-    print("File exists")
+    with open("user_login_info.txt", "r") as file:
+        email, password = file.read().splitlines()
+        db = UserDatabase()
+        if db.check_user(email, password):
+            subprocess.run(['python3', 'main_UI.py'])
+        else:
+            print('it does not work')
+            subprocess.run(['python3', 'login_UI.py'])
+
 except FileNotFoundError:
     subprocess.run(['python3', 'login_UI.py'])
---
+++ main_UI.py
@@ -0,0 +1,1 @@
+print('Yippie it works!')
