f9b78a102b6c8885ac7ae62103f7d2d4cf9cbf59
pestopasta74 <prestonwhiteman07@gmail.com>
2024-08-28 21:53:14 +0100

    commented the main file

--- main.py
+++ main.py
@@ -2,17 +2,22 @@ from modules.user_management import UserDatabase
 import login_UI
 import main_UI
 
-
+# Trying to read the user login information from a file
 try:
     with open("user_login_info.txt", "r") as file:
         email, password = file.read().splitlines()
 except FileNotFoundError:
     email, password = '', ''
 
+# Creating an instance of the UserDatabase class
 db = UserDatabase()
+
+# Checking if the user exists in the database
 if not db.check_user(email, password):
+    # If the user doesn't exist, display the login UI
     ui = login_UI
     ui.main()
     del ui
 
+# Display the main UI (as we now have a valid user)
 main_UI.main()
