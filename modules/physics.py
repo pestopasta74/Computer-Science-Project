fdfb2e14c83cdc49d7fda81536ee0818c57d8bd4
pestopasta74 <prestonwhiteman07@gmail.com>
2024-09-13 19:22:43 +0100

    tiny changes

---
+++ modules/physics.py
@@ -0,0 +1,7 @@
+class Physics:
+    def __init__(self, initial_velocity, final_velocity, acceleration, time):
+        self.gravity = 9.81
+        self.initial_velocity = initial_velocity
+        self.final_velocity = final_velocity
+        self.acceleration = acceleration
+        self.time = time
