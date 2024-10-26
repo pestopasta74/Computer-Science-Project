b39af17b0b63c5ca4a9a6b3564895bd1f723aa16
pestopasta74 <prestonwhiteman07@gmail.com>
2024-10-26 21:34:18 +0100

    Quiz finished - need to add GUI

--- modules/quiz_simulator.py
+++ modules/quiz_simulator.py
@@ -23,6 +23,13 @@ class Colours:
     gray = (128, 128, 128)
     orange = (255, 165, 0)
 
+def standard_form_converter(num):
+    """"Given a number in base units (e.g. 1.989e30), returns the number in standard form (e.g. 1.989 x 10^30)."""
+    num_str = f"{num:.2e}"
+    base, exponent = num_str.split('e')
+    return f"{base} x 10^{int(exponent)}"
+
+
 class PhysicsSimulator:
     def __init__(self):
         pass
@@ -99,15 +106,15 @@ class QuizSimulator:
         question_type = random.choice(question_types)
 
         if question_type == 'gravity':
-            question, answer = self.generate_gravity_question()
+            question, answer, units = self.generate_gravity_question()
         elif question_type == 'orbital_velocity':
-            question, answer = self.generate_orbital_velocity_question()
+            question, answer, units = self.generate_orbital_velocity_question()
         elif question_type == 'escape_velocity':
-            question, answer = self.generate_escape_velocity_question()
+            question, answer, units = self.generate_escape_velocity_question()
         else:
-            question, answer = self.generate_gravitational_field_strength_question()
+            question, answer, units = self.generate_gravitational_field_strength_question()
 
-        return question, answer
+        return question, answer, units
 
     def generate_gravity_question(self):
         use_real_values = random.choice([True, False])
@@ -115,15 +122,15 @@ class QuizSimulator:
         random_planet = random.choice(all_planets)
 
         m1 = random.uniform(1e22, 1e25) if not use_real_values else 1.989e30  # Mass of the Sun
-        m2 = random.uniform(1e22, 1e25) if not use_real_values else random_planet[2]
-        r = random.uniform(1e6, 1e9) if not use_real_values else random_planet[3]
+        m2 = random.uniform(1e22, 1e25) if not use_real_values else random_planet[2]  # Mass
+        r = random.uniform(1e6, 1e9) if not use_real_values else random_planet[3]  # Radius
         if use_real_values:
-            question = f"What is the gravitational force between the Sun (1.989e30 kg) and {random_planet[1]} ({random_planet[2]}kg)?"
+            question = f"What is the gravitational force between the Sun ({standard_form_converter(1.989e30)} kg) and {random_planet[1]} ({standard_form_converter(random_planet[2])} kg)?"
         else:
-            question = f"What is the gravitational force between two masses of {m1:.2e} kg and {m2:.2e} kg separated by {r:.2e} meters?"
+            question = f"What is the gravitational force between two masses of {standard_form_converter(m1)} kg and {standard_form_converter(m2)} kg separated by {standard_form_converter(r)} meters?"
         answer = self.physics_simulator.gravitational_force(m1, m2, r)
 
-        return question, answer
+        return question, answer, "N"
 
     def generate_orbital_velocity_question(self):
         use_real_values = random.choice([True, False])
@@ -131,55 +138,62 @@ class QuizSimulator:
         random_planet = random.choice(all_planets)
 
         M = random.uniform(1e30, 1e32) if not use_real_values else 1.989e30  # Mass of the Sun
-        r = random.uniform(1e8, 1e10) if not use_real_values else random_planet[3]
+        r = random.uniform(1e8, 1e10) if not use_real_values else random_planet[4]  # Orbital radius
         if use_real_values:
-            question = f"What is the orbital velocity of {random_planet[1]} around the Sun (1.989e30 kg) at a distance of {random_planet[3]} meters?"
+            question = f"What is the orbital velocity of {random_planet[1]} around the Sun ({standard_form_converter(1.989e30)} kg) at a distance of {standard_form_converter(random_planet[4])} meters?"
         else:
-            question = f"What is the orbital velocity of a body in orbit around a mass of {M:.2e} kg at a distance of {r:.2e} meters?"
+            question = f"What is the orbital velocity of a body in orbit around a mass of {standard_form_converter(M)} kg at a distance of {standard_form_converter(r)} meters?"
         answer = self.physics_simulator.orbital_velocity(M, r)
 
-        return question, answer
+        return question, answer, "m/s"
 
     def generate_escape_velocity_question(self):
         use_real_values = random.choice([True, False])
         all_planets = cursor.execute("SELECT * FROM planets WHERE name != 'Sun'").fetchall()
         random_planet = random.choice(all_planets)
 
-        M = random.uniform(1e30, 1e32) if not use_real_values else random_planet[2]
-        r = random.uniform(1e8, 1e10) if not use_real_values else random_planet[3]
+        M = random.uniform(1e30, 1e32) if not use_real_values else random_planet[2]  # Mass
+        r = random.uniform(1e8, 1e10) if not use_real_values else random_planet[3]  # Radius
         if use_real_values:
-            question = f"What is the escape velocity from {random_planet[1]} ({random_planet[2]} kg) at a distance of {random_planet[3]} meters?"
+            question = f"What is the escape velocity from {random_planet[1]} ({standard_form_converter(random_planet[2])} kg) at a distance of {standard_form_converter(random_planet[3])} meters?"
         else:
-            question = f"What is the escape velocity from a mass of {M:.2e} kg at a distance of {r:.2e} meters?"
+            question = f"What is the escape velocity from a mass of {standard_form_converter(M)} kg at a distance of {standard_form_converter(r)} meters?"
         answer = self.physics_simulator.escape_velocity(M, r)
 
-        return question, answer
+        return question, answer, "m/s"
 
     def generate_gravitational_field_strength_question(self):
         use_real_values = random.choice([True, False])
         all_planets = cursor.execute("SELECT * FROM planets WHERE name != 'Sun'").fetchall()
         random_planet = random.choice(all_planets)
 
-        m1 = random.uniform(1e22, 1e25) if not use_real_values else random_planet[2]
-        r = random.uniform(1e6, 1e9) if not use_real_values else random_planet[3]
+        m1 = random.uniform(1e22, 1e25) if not use_real_values else random_planet[2]  # Mass
+        r = random.uniform(1e6, 1e9) if not use_real_values else random_planet[3]  # Radius
         if use_real_values:
-            question = f"What is the gravitational field strength at a distance of {random_planet[3]} meters from {random_planet[1]} ({random_planet[2]} kg)?"
+            question = f"What is the gravitational field strength at a distance of {standard_form_converter(random_planet[3])} meters from {random_planet[1]} ({standard_form_converter(random_planet[2])} kg)?"
         else:
-            question = f"What is the gravitational field strength at a distance of {r:.2e} meters from a mass of {m1:.2e} kg?"
+            question = f"What is the gravitational field strength at a distance of {standard_form_converter(r)} meters from a mass of {standard_form_converter(m1)} kg?"
         answer = self.physics_simulator.gravitational_field_strength(m1, r)
 
-        return question, answer
+        return question, answer, "N/kg"
+
+    def user_input_to_float(self, user_input):
+        """Allows converting 10e10 or 1x10^10 or 1*10^-10 to a float."""
+        user_input = user_input.replace('x', '*').replace('^', '**')
+        number, exponent = user_input.split('*10**')
+        return float(number) * 10 ** int(exponent)
 
     def ask_question(self):
-        # Example of a quiz question
-        question = "What is the gravitational force between two masses of 5.97e24 kg and 7.35e22 kg separated by 384.4e6 meters?"
+        question, answer, units = self.generate_question()
         print(question)
+        user_answer = input("Your answer: ")
+        formatted = self.user_input_to_float(user_answer)
+        isEqual = abs(formatted - answer) < 0.01
+        print(f"Simulated Answer: {standard_form_converter(answer)} {units}")
+        print(f"Your Answer: {user_answer} {'Correct' if isEqual else 'Incorrect'}")
 
-        # Simulate the answer using PhysicsSimulator
-        answer = self.physics_simulator.simulate_answer(
-            'gravity', m1=5.97e24, m2=7.35e22, r=384.4e6)
 
-        print(f"Simulated Answer: {answer:.2e} N")
+
 
 # Example usage:
 if __name__ == "__main__":
